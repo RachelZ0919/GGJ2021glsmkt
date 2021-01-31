46ee48a1e62344dc3c810b4a8cbe6cee6c755799
gmQiu <785133766@qq.com>
2021-01-31 15:50:08 +0800

    Add EndScene and Win

---
+++ Assets/Resources/Scripts/Boss/Boss.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using DragonBones;
+
+
+public class Boss : MonoBehaviour
+{
+    private UnityArmatureComponent armatureComponent;
+
+    private void Awake()
+    {
+        armatureComponent = transform.Find("anim").GetComponent<UnityArmatureComponent>();
+        GetComponent<Stats>().OnStatsChanged += CheckDeath;
+    }
+
+    private void CheckDeath(Stats stat)
+    {
+        if(stat.health <= 0)
+        {
+            stat.OnStatsChanged -= CheckDeath;
+            armatureComponent.AddDBEventListener(EventObject.LOOP_COMPLETE, OnDeathAnimationEnd);
+            armatureComponent.animation.Play("die");
+        }
+    }
+    private void OnDeathAnimationEnd(string type, EventObject eventObject)
+    {
+        if (eventObject.animationState.name == "die")
+        {
+            armatureComponent.animation.GotoAndStopByProgress("die", 1);
+            GameManager.instance.LevelSucceed();
+            armatureComponent.RemoveDBEventListener(EventObject.LOOP_COMPLETE, OnDeathAnimationEnd);
+
+        }
+    }
+}
