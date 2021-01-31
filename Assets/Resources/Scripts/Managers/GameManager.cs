3f9dfa1058d39bc5c94231c97896e39df01322c7
gmQiu <785133766@qq.com>
2021-01-31 09:57:42 +0800

    Add GameManager

--- Assets/Resources/Scripts/Managers/GameManager.cs
+++ Assets/Resources/Scripts/Managers/GameManager.cs
@@ -10,10 +10,45 @@ public class GameManager : MonoBehaviour
         if(instance == null)
         {
             instance = this;
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            player.GetComponent<Stats>().OnStatsChanged += CheckHP;
+            GameObject boss = GameObject.FindGameObjectWithTag("Enemy");
+            boss.GetComponent<Stats>().OnStatsChanged += CheckHP;
+
         }
         else
         {
             Destroy(this);
         }
     }
+
+    public void LevelFailed()
+    {
+        
+    }
+
+
+    public void LevelSucceed()
+    {
+
+    }
+
+    private void CheckHP(Stats stat)
+    {
+        if (stat.CompareTag("Player"))
+        {
+            if (stat.health <= 0)
+            {
+                LevelFailed();
+            }
+        }
+        else if (stat.CompareTag("Enemy"))
+        {
+            if (stat.health <= 0)
+            {
+                LevelSucceed();
+            }
+        }
+    }
+
 }
